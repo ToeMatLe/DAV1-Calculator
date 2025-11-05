7d3462c39a08ec67605c2197383fc97d4cd6b9b7
Thomas Le <142299866+ToeMatLe@users.noreply.github.com>
2025-11-05 15:12:07 -0800

    Fix digit representation in sevenSegDigit.sv

--- sevenSegDigit.sv
+++ sevenSegDigit.sv
@@ -6,34 +6,34 @@ output logic [6:0] displayBits
 );
 always_comb begin 
   case(digit)
-    0000: begin 
+    4'b0000: begin 
       displayBits = 0111111; //0
     end
-    0001: begin 
+    4'b0001: begin 
       displayBits = 0000110; //1
     end
-    0010: begin 
+    4'b0010: begin 
       displayBits = 1011011; //2
     end
-    0011: begin 
+    4'b0011: begin 
       displayBits = 1001111; //3
     end
-    0100: begin 
+    4'b0100: begin 
       displayBits = 1100110; //4
     end
-    0101: begin 
+    4'b0101: begin 
       displayBits = 1101101; //5
     end
-    0110: begin 
+    4'b0110: begin 
       displayBits = 1111101; //6
     end
-    0111: begin 
+    4'b0111: begin 
       displayBits = 0000111; //7
     end
-    1000: begin 
+    4'b1000: begin 
       displayBits = 1111111; //8
     end
-    1001: begin 
+    4'b1001: begin 
       displayBits = 1110111; //9
     end
     default: displayBits = 0000000; //Nothing
