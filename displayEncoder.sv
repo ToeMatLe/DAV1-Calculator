06c69a7986eee002839a5d8db051a9aa166890bb
Thomas Le <142299866+ToeMatLe@users.noreply.github.com>
2025-11-04 18:21:15 -0800

    Add displayEncoder module for number encoding
    Used modulus by 10 to separate 4 digit displays

---
+++ displayEncoder.sv
@@ -0,0 +1,18 @@
+module displayEncoder (
+  input logic [18:0] result,
+  output logic [3:0] numberBits [0:3]
+);
+int temp;
+always_comb begin 
+  temp = result;
+  numberBits[0] = temp % 10;
+  temp = temp/10;
+  numberBits[1] = temp % 10;
+  temp = temp/10;
+  numberBits[2] = temp % 10;
+  temp = temp/10;
+  numberBits[3] = temp % 10;
+end
+endmodule
+
+  
