7928a65a06aaf2d883fba7683f2d4471709dcf8e
Thomas Le <142299866+ToeMatLe@users.noreply.github.com>
2025-11-05 15:28:32 -0800

    Change displayBits to 7-bit representation

--- sevenSegDigit.sv
+++ sevenSegDigit.sv
@@ -7,36 +7,36 @@ output logic [6:0] displayBits
 always_comb begin 
   case(digit)
     4'b0000: begin 
-      displayBits = 0111111; //0
+      displayBits = 7'b0111111; //0
     end
     4'b0001: begin 
-      displayBits = 0000110; //1
+      displayBits = 7'b0000110; //1
     end
     4'b0010: begin 
-      displayBits = 1011011; //2
+      displayBits = 7'b1011011; //2
     end
     4'b0011: begin 
-      displayBits = 1001111; //3
+      displayBits = 7'b1001111; //3
     end
     4'b0100: begin 
-      displayBits = 1100110; //4
+      displayBits = 7'b1100110; //4
     end
     4'b0101: begin 
-      displayBits = 1101101; //5
+      displayBits = 7'b1101101; //5
     end
     4'b0110: begin 
-      displayBits = 1111101; //6
+      displayBits = 7'b1111101; //6
     end
     4'b0111: begin 
-      displayBits = 0000111; //7
+      displayBits = 7'b0000111; //7
     end
     4'b1000: begin 
-      displayBits = 1111111; //8
+      displayBits = 7'b1111111; //8
     end
     4'b1001: begin 
-      displayBits = 1110111; //9
+      displayBits = 7'b1110111; //9
     end
-    default: displayBits = 0000000; //Nothing
+    default: displayBits = 7'b0000000; //Nothing
   endcase
 end
 endmodule
